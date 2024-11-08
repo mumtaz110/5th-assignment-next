6e5129d2be4e4ec0025a64302cff191e06f27fe4
mumtaz110 <mtzak786@gmail.com>
2024-11-08 10:29:58 +0500

    first commit

---
+++ app/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Header() {
+  return <div className="container-mx-auto flex justify-between items-center bg-[#A29875] h-[134px] w-[1920px]  ">
+    <h1 className="text-[#FFFFFF;] font-bold font-Rye text-6xl ">MANZZARI</h1>
+    </div>;
+  
+}
+
+export default Header;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,9 +11,11 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        
       },
     },
   },
   plugins: [],
 };
+
 export default config;
